6aef4205bf987061a156a94665cbd488a6b52b8b
Kripa432 <iamkripashanker@gmail.com>
2017-10-23 00:41:17 +0530

    Google chrome from repository

--- freshInstall.sh
+++ freshInstall.sh
@@ -47,9 +47,7 @@ echo "Installing gimp"
 sudo apt-get install -y gimp
 
 echo "Installing google chrome "
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-rm google-chrome-stable_current_amd64.deb
+sudo apt-get install google-chrome-stable
 
 #echo "Installing virtualbox"
 #sudo apt-get install -y virtualbox 
